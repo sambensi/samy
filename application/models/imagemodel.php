1f9a5ac2b22135597329246a22a333b8f725b6ee
Jacques Bodin-Hullin <jacques@bodin-hullin.net>
2012-12-15 22:58:06 +0100

    Ajout du modèle "ImageModel"
    + Ajout des "public" devant les méthodes
    + Modification du constructeur
    + Modification du nom de la variable $table en $_table : on préfixe
    toujours par un _ les variables privées ou protégées

---
+++ application/models/imagemodel.php
@@ -0,0 +1,61 @@
+<?php
+
+class ImageModel extends CI_Model
+{
+
+    /**
+     * Nom de la table en BDD
+     */
+    private $_table = 'images';
+
+    /**
+     * Constructeur du modèle
+     */
+    public function __construct()
+    {
+        // On appel le parent
+        parent::__construct();
+    }
+
+    /**
+     * Récupère toutes les images
+     */
+    public function getAll()
+    {
+        $query = $this->db->get($this->_table);
+        return $query->result();
+    }
+
+    /**
+     * Recherche
+     */
+    public function query($q, $order)
+    {
+        $this->db->like('comment', $q);
+        $this->db->or_like('path', $q);
+        $this->db->from($this->_table);
+        if ($order) {
+            $this->db->order_by($order);
+        }
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    /**
+     * Récupère une seule image
+     */
+    public function get($id)
+    {
+        $query = $this->db->get_where($this->_table, array('id' => $id));
+        return $query->result();
+    }
+
+    /**
+     * Supprime une image
+     */
+    public function delete($id)
+    {
+        $this->db->delete($this->_table, array('id' => $id));
+    }
+
+}
