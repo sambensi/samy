e8b13b3dc65f7f1a13dc61e767eb0158bd23fee0
BENSIDHOUM Samy <samy.bensidhoum@live.fr>
2012-12-18 12:49:50 +0100

    changement de couleur pour la recommandation !

--- js/scriptColor.js
+++ js/scriptColor.js
@@ -1,13 +1,39 @@
 /*alert('coucou');
  */
- 
- $$('test').each(function(el){
- 	el.addEvents({
-	 	mouseover: function(){
-	        alert('mouseover');
-	    },
-	    click: function(){
-	        alert('click');
-	    }
- 	});
-});
+var color=new Array('white','green','red');
+
+var test = function(e){
+	var idEnCours = this.get('id');
+	var style = this.getStyle('background-color');
+	
+	if( style == '#ffffff' ){
+		this.setStyles({
+	    	border: '1px solid green',
+    		backgroundColor: 'green',
+		})
+	}
+	if( style == 'green' ){
+		this.setStyles({
+	    	border: '1px solid red',
+    		backgroundColor: 'red',
+		})
+	}
+	if( style == 'red' ){
+		this.setStyles({
+	    	border: '1px solid #ffffff',
+    		backgroundColor: '#ffffff',
+		})
+	}
+};
+
+window.addEvent('domready', function(){
+
+	$$('div').each(function(el){
+		
+		
+		el.addEvents({
+		 	click: test
+		 	});
+	});
+	
+}); 
